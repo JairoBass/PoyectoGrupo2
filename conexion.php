ef28e679fd7722057b88da984f9408b635f209c0
SacatoroAlex <88003223+SacatoroAlex@users.noreply.github.com>
2022-01-21 15:45:03 -0600

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server="localhost";
-$user="root";
-$password="";
-$nameBD="restaurante";
-$conexion = mysqli_connect($server,$user,$password,$nameBD);
-mysqli_query($conexion,"SET NAMES 'UTF-8'");
-?>
